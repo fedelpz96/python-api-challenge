154de305dc090db8e50a04cd2f155aff746177bc
Federico Lopez <fedelpz96@github.com>
2020-07-31 13:55:16 -0500

    Final

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "db87a9815404dd5ec75f967d56501518"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCx7y99PIfGTjGylloEexNGW2GTiCqOH6E"
